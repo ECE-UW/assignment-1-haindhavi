6e30f4142e59f8f2bbeefb9030bee55a866120d5
Haindhavi Gutta <haindhavi.g@gmail.com>
2019-09-26 18:33:32 -0400

    updated input loop to use street database

--- a1ece650.py
+++ a1ece650.py
@@ -1,22 +1,42 @@
 import sys
 
-# YOUR CODE GOES HERE
+from command_parser import parse
+from street_database import StreetDatabase
+
 
 def main():
-    ### YOUR MAIN CODE GOES HERE
-
-    ### sample code to read from stdin.
-    ### make sure to remove all spurious print statements as required
-    ### by the assignment
+    db = StreetDatabase()
     while True:
         line = sys.stdin.readline()
-        if line == '':
+        if line == "" or line == None:
             break
-        print 'read a line:', line
 
-    print 'Finished reading input'
-    # return exit code 0 on successful termination
+        try:
+            p = parse(line)
+            print (p)
+
+            if p["command"] == "ADD_STREET":
+                db.add_street(p["street_name"], p["coordinates"])
+                db.print_db()
+
+            if p["command"] == "CHANGE_STREET":
+                db.change_street(p["street_name"], p["coordinates"])
+                db.print_db()
+
+            if p["command"] == "REMOVE_STREET":
+                db.delete_street(p["street_name"])
+                db.print_db()
+
+            if p["command"] == "GENERATE_GRAPH":
+                print ("GENERATE_GRAPH: TO BE IMPLEMENTED")
+                print ""
+
+        except:
+            sys.stderr.write("Error: Command parser doesn't work as expected\n")
+            raise
+
     sys.exit(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
